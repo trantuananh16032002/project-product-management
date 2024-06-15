1253344b9b94f889eabfc8a1fc9b00ec58b79163
RamenHoang <anhnguyenhoang321@gmail.com>
2024-06-16 00:20:41 +0700

    Fix issue radomize to same token

--- helpers/generate.js
+++ helpers/generate.js
@@ -1,10 +1,12 @@
 module.exports.generateRandomString = (length) => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result = "";
+    const nowInFormatYmdHis = new Date().toISOString().replace(/[-:T]/g, "").split(".")[0];
+    const lengthOfNowInFormatYmdHis = nowInFormatYmdHis.length;
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length)); 
     }
-    return result;
+    return result + nowInFormatYmdHis;
 };
 module.exports.generateRandomNumber = (length) => {
     const characters = "0123456789";
